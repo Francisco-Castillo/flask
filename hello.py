ccc7d2816fcd5f1609dc94ebbbc5ae3f8ff58f72
francisco castillo <cefrancastillo@gmail.com>
2019-07-20 20:11:31 -0300

    flask routing

--- hello.py
+++ hello.py
@@ -3,22 +3,31 @@ from flask import Flask
 
 app =Flask(__name__)
 
-OLE_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEED ={'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
+           'ole': 'https://www.ole.com.ar/rss/river-plate/'}
 
 @app.route("/")
-def get_news():
-    feed = feedparser.parse(OLE_FEED)
+def get_ole():
+    return get_news('ole')
+
+@app.route("/bbc")
+def get_bbc():
+    return get_news('bbc')
+
+
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEED[publication])
     first_article = feed['entries'][0]
     return """
         <html>
             <body>
-                <h1>BBC Headlines</h1>
+                <h1>Headlines</h1>
                 <b>{0}</b><br/>
                 <i>{1}</i><br/>
                 <p>{2}</p><br/>
             </body>
         </html>
-    """.format(first_article.get("title"), first_article.get("published"),first_article.get("summary"))
+    """.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
