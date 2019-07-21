3fa705ce47fe35c0d72bf713cbf5af17525e3e84
francisco castillo <cefrancastillo@gmail.com>
2019-07-21 20:33:52 -0300

    jinja templates, manejo de objetos

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import feedparser
 from flask import Flask
-
+from flask import render_template
 app =Flask(__name__)
 
 RSS_FEED ={'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
@@ -14,20 +14,12 @@ def get_ole():
 def get_bbc():
     return get_news('bbc')
 
-
-def get_news(publication):
+@app.route("/")
+@app.route("/<publication>")
+def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEED[publication])
     first_article = feed['entries'][0]
-    return """
-        <html>
-            <body>
-                <h1>Headlines</h1>
-                <b>{0}</b><br/>
-                <i>{1}</i><br/>
-                <p>{2}</p><br/>
-            </body>
-        </html>
-    """.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+    return render_template("home.html", article=first_article)
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
