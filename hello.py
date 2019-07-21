848fce51262ff8699a6b9a615c400b2dfe3742c9
francisco castillo <cefrancastillo@gmail.com>
2019-07-21 20:54:59 -0300

    flask request

--- hello.py
+++ hello.py
@@ -1,22 +1,27 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
+
 app =Flask(__name__)
 
 RSS_FEED ={'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
            'ole': 'https://www.ole.com.ar/rss/river-plate/'}
 
-@app.route("/")
-def get_ole():
-    return get_news('ole')
+
 
 @app.route("/bbc")
 def get_bbc():
     return get_news('bbc')
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEED:
+        publication="ole"
+    else:
+        publication = query.lower()
+
     feed = feedparser.parse(RSS_FEED[publication])
     return render_template("home.html", articles=feed['entries'])
 
